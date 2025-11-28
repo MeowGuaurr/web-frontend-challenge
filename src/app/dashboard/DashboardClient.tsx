b81fe1e31351909828d9ad721a866d7fa67b5a98
MeowGuaurr <Reynitamercedes@gmail.com>
2025-11-27 23:44:57 -0600

    edit dashboard

---
+++ src/app/dashboard/DashboardClient.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const DashboardApp = dynamic(() => import('./DashboardApp'), {
+  ssr: false,
+});
+
+export default function DashboardClient() {
+  return <DashboardApp />;
+}
